2d54a4edb3516e7607e7213b0ee0a5eb1f613613
alidaka <alidaka@gmail.com>
2020-05-18 10:12:11 -0700

    udev: incremental progress and debugging

--- upload.sh
+++ upload.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
+echo "jabberwock"
+echo
+echo
+env >> ~/code/fit-upload/udev.log
+
 echo "Uploading to Google Drive..."
 #./out/gdrive
 
 echo "Uploading to Strava..."
-for filename in /media/augustus/GARMIN/GARMIN/ACTIVITY/*.FIT; do
-  echo "Uploading $filename..."
-  curl -X POST https://www.strava.com/api/v3/uploads \
-    -H "Authorization: Bearer abcd123" \
-    -F data_type="fit" \
-    -F file=@$filename
-done
+#/media/augustus/GARMIN/GARMIN/ACTIVITY/*.FIT
 
-xdg-open https://www.strava.com/athlete/training
+#xdg-open https://www.strava.com/athlete/training
